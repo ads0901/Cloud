5227d16938bd14cf87bf96edd4245a1a60cb7391
ads0901 <30352741+ads0901@users.noreply.github.com>
2019-11-07 17:17:17 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,46 +1,151 @@
-variable "azure_resource_group" {
-  type        = "string"
-  description = "Name of the Azure Resource Group."
-
-  default = "AvanadeTerraformRG"
+#Common Variables
+variable "location" {
+  description = "Location of the resources"
+  type = "string"
 }
-
-variable "azure_location" {
-  type        = "string"
-  description = "Name of the Azure Location."
-
-  default = "australiaeast"
+variable "vnetname" {
+  description = "Virtual Network for the SAP Group Deployment"
+  type = "string"
 }
-
-variable "azure_appsvcplan" {
-  type        = "string"
-  description = "Name of the Azure App Service Plan."
-
-  default = "abcd2019-appsvcplan"
+variable "vnetrg" {
+  description = "Virtual Network Resource Group"
+  type = "string"
 }
-
-variable "azure_appsvcact" {
-  type        = "string"
-  description = "Name of the Azure App Service Account"
-
-  default = "abcd2019-app-svc"
+variable "mgmtrg" {
+  description = "Name of the Management Resource Group"
+  type = "string"
 }
-variable "azure_stg_account" {
-  type        = "string"
-  description = "Name of the Azure Storage Account"
-
-  default = "avanadestorageactname"
+variable "vmadmin" {
+  description = "Local Server Admin"
+  type = "string"
 }
-variable "azure_stg_container" {
-  type        = "string"
-  description = "Name of the Azure Storage Container"
-
-  default = "kgavterracontainer"
+variable "vmadminpassword" {
+  description = "Local Server Admin Password"
+  type = "string"
 }
-variable "username" {  
-  default = "avanade"  
-}  
-  
-variable "password" {  
-  default = "Password!1234"  
-}  
+variable "costcenter" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "environmentinfo" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "ownerinfo" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "applicationname" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "platform" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "deploymenttype" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "notificationdistlist" {
+  description = "Required Tag"
+  type = "string"
+}
+variable "trproductid" {
+  description = "Required Tag"
+  type = "string"
+}
+#########################################################################################
+#SAP App Server Variables
+variable "app-nodecount" {
+  description = "Number of SAP App Servers to Deploy"
+  type = "string"
+  }
+variable "app-vmsize" {
+  description = "Size of the SAP App Servers Virtual Macines"
+  type = "string"
+}
+variable "app-subnet" {
+  description = "Subnet for the SAP App Servers"
+  type = "string"
+}
+variable "app-rg" {
+  description = "Resource Group Name for the SAP App Servers"
+  type = "string"
+}
+variable "app-avset" {
+  description = "Availability Set Name for the SAP App Servers"
+  type = "string"
+}
+variable "app-name" {
+  description = "Name for the SAP App Servers"
+  type = "string"
+}
+variable "app-policy" {
+  description = "Backup Policy for the SAP App Servers"
+  type = "string"
+}
+#########################################################################################
+#SAP Central Services Server Variables
+variable "cs-vmsize" {
+  description = "Size of the SAP Central Services Servers"
+  type = "string"
+}
+variable "cs-subnet" {
+  description = "Subnet for the SAP Central Services Servers"
+  type = "string"
+}
+variable "cs-nodecount" {
+  description = "Number of SAP Central Services Servers to Deploy"
+  type = "string"
+}
+variable "cs-rg" {
+  description = "Resource Group Name for the SAP Central Services Servers"
+  type = "string"
+}
+variable "cs-avset" {
+  description = "Availability Set Name for the SAP Central Services Servers"
+  type = "string"
+}
+variable "cs-lb" {
+  description = "Availability Set Name for the SAP Central Services Servers"
+  type = "string"
+}
+variable "cs-name" {
+  description = "Name of SAP Central Services Servers"
+  type = "string"
+}
+variable "cs-policy" {
+  description = "Backup Policy for the SAP Central Services Servers"
+  type = "string"
+}
+#########################################################################################
+#SAP Hana DB Server Variables
+variable "db-vmsize" {
+  description = "Size of the SAP Hana DB Servers"
+  type = "string"
+}
+variable "db-subnet" {
+  description = "Subnet for the SAP Hana DB Servers"
+  type = "string"
+}
+variable "db-nodecount" {
+  description = "Number of SAP Hana DB Servers to Deploy"
+  type = "string"
+}
+variable "db-rg" {
+  description = "Resource Group Name for the SAP Hana DB Servers"
+  type = "string"
+}
+variable "db-avset" {
+  description = "Availability Set Name for the SAP Hana DB Servers"
+  type = "string"
+}
+variable "db-lb" {
+  description = "Availability Set Name for the SAP Hana DB Servers"
+  type = "string"
+}
+variable "db-name" {
+  description = "Name of SAP Hana DB Servers"
+  type = "string"
+}
